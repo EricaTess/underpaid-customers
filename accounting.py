cf5781ceedc89ac696b8a7cb8d0353c599303298
Erica Sanchez <sanchez.ericat@gmail.com>
2020-05-01 00:00:32 +0000

    added comments

--- accounting.py
+++ accounting.py
@@ -1,43 +1,17 @@
 
-
-melon_cost = 1.00
-
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
-
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
-
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
-
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
-
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
-
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
-
-
-
-
 def who_underpaid(customer_payments):
   '''calculate cost of melons and see who underpaid'''
 
-
+  #defined txt file
   payments = open(customer_payments)
 
+  #iterate through all lines in file
   for line in payments:
+
+    #split lines into lists
     order = line.split('|')
 
+    #defined first name
     full_name = order[1]
 
     first_name = full_name.split(' ')[0]
@@ -47,13 +21,14 @@ def who_underpaid(customer_payments):
 
     paid = float(order[3])
 
-
+    #calculate the expected cost of melons
     expected = melons * melon_cost
     if expected != paid:
       print(f"{first_name} paid ${paid:.2f},",
             f"expected ${expected:.2f}"
           )
 
+      #state whether a customer overpaid or underpaid
       if expected < paid:
         print(f"{first_name} has overpaid for their melons.")
 
